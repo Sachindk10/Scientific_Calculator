544db9884ab995ccf430bf6a94c9ac29679656d6
SACHIN DK <sachindk52@gmail.com>
2023-04-12 02:21:47 +0530

    added with basic functionalities

---
+++ src/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+
+    let currentValue = "";
+
+    function evaluateResult() {
+        console.log('currentValue:', currentValue);
+        const convertedValue = currentValue.replace("×", "*")
+        .replace("÷", "/")
+        .replace('%', '*0.01')
+
+        console.log('convertedValue:', convertedValue);
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        display.value = currentValue;
+    }
+
+    for(let i = 0; i<buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function() {
+            const value = button.innerText;
+
+            try {
+                if (value == 'AC') {
+                    currentValue = "";
+                    display.value = currentValue;
+                } 
+                else if(value == "=") {
+                    evaluateResult();
+                }
+                else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            }
+            catch (error) {
+                console.log(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
+            }
+        })
+    }
+
+})
